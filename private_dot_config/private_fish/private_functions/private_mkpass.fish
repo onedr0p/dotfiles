94f7ac266217d9291ab4c5371de3faf7ba0c7ea1
ᗪєνιη ᗷυнʟ <onedr0p@users.noreply.github.com>
2021-04-04 08:48:38 -0400

    Update private_mkpass.fish

--- private_dot_config/private_fish/private_functions/private_mkpass.fish
+++ private_dot_config/private_fish/private_functions/private_mkpass.fish
@@ -1,3 +1,3 @@
 function mkpass --description 'create a random 20 character string'
-    openssl rand -base64 20 | fold -w1 | shuf | tr -d '\n' | clipp; pastee;
+    openssl rand -base64 20 | fold -w1 | shuf | tr -d '\n' | clipy; pastey;
 end
