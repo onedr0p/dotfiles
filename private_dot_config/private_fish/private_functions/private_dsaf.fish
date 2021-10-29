797529628354bac253cfa2a5a7201aeba618d0cb
Devin Buhl <devin@buhl.casa>
2021-10-29 12:47:00 -0400

    Update dootphiles

--- private_dot_config/private_fish/private_functions/private_dsaf.fish
+++
@@ -1,3 +0,0 @@
-function dsaf --description 'nuke all docker data'
-    docker system prune -a --force
-end
--- private_dot_config/private_fish/private_functions/private_vdy.fish
+++
@@ -1,3 +0,0 @@
-function vdy --description 'modern watch'
-    viddy -d -n 1 --shell fish $argv[1..-1]
-end
