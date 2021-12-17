a311d66132138f7ff38d55465c4063ffa34d4892
Devin Buhl <devin@buhl.casa>
2021-12-17 16:20:50 -0500

    fix(commit): now accounts for breaking change char
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- private_dot_config/private_fish/private_functions/private_commit.fish
+++ private_dot_config/private_fish/private_functions/private_commit.fish
@@ -1,13 +1,18 @@
 # My some-what useful commit function
-# Typing `commit feat terraform update lock file` will become
-# `feat(terraform): update lock file`
+# Typing `commit feat! terraform update lock file` will become
+# `feat(terraform)!: update lock file`
 function commit --description 'git conventional commits'
     set commit_message
     if count $argv > /dev/null
         set commit_type $argv[1]
+        set breaking_change_char ""
+        if string match '*!' $commit_type
+            set breaking_change_char "!"
+            set commit_type (string trim --chars="!" $commit_type)
+        end
         set commit_scope $argv[2]
         set commit_description (string join " " $commit_description $argv[3..-1])
-        set commit_message "$commit_type($commit_scope): $commit_description"
+        set commit_message "$commit_type($commit_scope)$breaking_change_char: $commit_description"
         git commit -s -m "$commit_message"
     else
         set whatthecommit (curl -s http://whatthecommit.com/index.txt)
