1600381a42aa709d75a0bfe826baf52a66946b51
Devin Buhl <onedr0p@users.noreply.github.com>
2025-04-06 19:10:32 -0400

    Create 10-homebrew.fish

---
+++ dot_config/fish/conf.d/10-homebrew.fish
@@ -0,0 +1,28 @@
+set -gx HOMEBREW_NO_ANALYTICS 1
+set -gx HOMEBREW_BUNDLE_FILE $XDG_CONFIG_HOME/homebrew/brewfile
+set -gx HOMEBREW_CASK_OPTS --no-quarantine
+set -gx HOMEBREW_AUTO_UPDATE_SECS 86400
+
+for bindir in /usr/local/bin /opt/homebrew/bin
+  test -d $bindir && fish_add_path --global --prepend $bindir
+end
+
+type -q brew && eval (brew shellenv)
+
+fish_add_path --global --prepend /opt/homebrew/opt/curl/bin
+
+if type -q gdu
+  fish_add_path --global --prepend $HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin
+end
+
+if type -q gsed
+  fish_add_path --global --prepend $HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin
+end
+
+if type -q gawk
+  fish_add_path --global --prepend $HOMEBREW_PREFIX/opt/gawk/libexec/gnubin
+end
+
+if type -q gfind
+  fish_add_path --global --prepend /opt/homebrew/opt/findutils/libexec/gnubin
+end
