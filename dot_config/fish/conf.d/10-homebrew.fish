acafd9d15181d5049368083cf0a6ccfe8d8e9aa5
Devin Buhl <onedr0p@users.noreply.github.com>
2025-04-06 19:15:38 -0400

    Update 10-homebrew.fish

--- dot_config/fish/conf.d/10-homebrew.fish
+++ dot_config/fish/conf.d/10-homebrew.fish
@@ -5,7 +5,7 @@ set -gx HOMEBREW_AUTO_UPDATE_SECS 86400
 set -gx MISE_FISH_AUTO_ACTIVATE 0
 
 for bindir in /usr/local/bin /opt/homebrew/bin
-  test -d $bindir && fish_add_path --global --prepend $bindir
+    test -d $bindir && fish_add_path --global --prepend $bindir
 end
 
 type -q brew && eval (brew shellenv)
@@ -13,17 +13,17 @@ type -q brew && eval (brew shellenv)
 fish_add_path --global --prepend /opt/homebrew/opt/curl/bin
 
 if type -q gdu
-  fish_add_path --global --prepend $HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin
+    fish_add_path --global --prepend $HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin
 end
 
 if type -q gsed
-  fish_add_path --global --prepend $HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin
+    fish_add_path --global --prepend $HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin
 end
 
 if type -q gawk
-  fish_add_path --global --prepend $HOMEBREW_PREFIX/opt/gawk/libexec/gnubin
+    fish_add_path --global --prepend $HOMEBREW_PREFIX/opt/gawk/libexec/gnubin
 end
 
 if type -q gfind
-  fish_add_path --global --prepend /opt/homebrew/opt/findutils/libexec/gnubin
+    fish_add_path --global --prepend /opt/homebrew/opt/findutils/libexec/gnubin
 end
