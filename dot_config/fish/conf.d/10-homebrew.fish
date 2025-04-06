d0fbc3afbb8e570afe8f9990bd742f3e9cf7ee56
Devin Buhl <onedr0p@users.noreply.github.com>
2025-04-06 19:14:01 -0400

    Update 10-homebrew.fish

--- dot_config/fish/conf.d/10-homebrew.fish
+++ dot_config/fish/conf.d/10-homebrew.fish
@@ -2,6 +2,7 @@ set -gx HOMEBREW_NO_ANALYTICS 1
 set -gx HOMEBREW_BUNDLE_FILE $XDG_CONFIG_HOME/homebrew/brewfile
 set -gx HOMEBREW_CASK_OPTS --no-quarantine
 set -gx HOMEBREW_AUTO_UPDATE_SECS 86400
+set -gx MISE_FISH_AUTO_ACTIVATE 0
 
 for bindir in /usr/local/bin /opt/homebrew/bin
   test -d $bindir && fish_add_path --global --prepend $bindir
